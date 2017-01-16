d1fe25960e2b242a8092ad554d51438954a06b37
Yonathan117 <yonathanapriyanto117@gmail.com>
2017-01-16 14:42:05 +0700

    Update peta1.php

--- peta1.php
+++ peta1.php
@@ -73,12 +73,12 @@
           mapTypeId: 'roadmap'
         });
 
-        // Create the search box and link it to the UI element.
+        // membuat search box.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
+        // hasil dari searchbox.
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
         });
