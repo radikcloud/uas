6761a90c7aac2ecf50db4e72b230d88e7a5fa666
radikcloud <cahyoradik99@gmail.com>
2017-01-16 14:10:37 +0700

    Create peta1.php

---
+++ peta1.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Maps Biasa</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+	  
+	   </style>
+  </head>
+  
+  <body>
+    <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+    <div id="map"></div>
